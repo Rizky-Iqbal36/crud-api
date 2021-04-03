86d4cedbb726fdd36ecbe4694193f64faab27ab9
rizkyiqbal <rizkiiqbal36@gmail.com>
2021-04-03 12:05:02 +0700

    perf: ⚡️ prepare for development

---
+++ src/app/exceptions/httpException.ts
@@ -0,0 +1,19 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export class CustomException extends HttpException {
+  constructor(httpStatus: number, flag: string, options?) {
+    super(
+      {
+        flag,
+        options,
+      },
+      httpStatus,
+    );
+  }
+}
+
+export class SuccessResponse extends HttpException {
+  constructor(data: any) {
+    super(data, HttpStatus.OK);
+  }
+}
